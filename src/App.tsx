f074d3e1e46df37d2ef4e873d113dfc117ad2d00
shahin2525 <shahin451999@gmail.com>
2024-03-03 13:01:32 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>i ma from todo</h1>
+      <Button>My button</Button>
+    </>
+  );
+}
+
+export default App;
